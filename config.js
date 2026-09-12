48320944126a902684235aee75e7625476c01334
Arefin leno <arefinanwar112@gmail.com>
2026-09-12 23:13:40 +0600

    Point the pilot at the Google Sheet web app

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.HUMAN_EVAL = {
   // Paste your Apps Script web-app URL here after you deploy (ends with /exec).
-  scriptUrl: "",
+  scriptUrl: "https://script.google.com/macros/s/AKfycbxO7gXJ7inMTG1spdTfFlveE-AiIJWMEJX4Cp2ZLodAq4iF0bqe3SvhbUmjqBJu0E-LLg/exec",
 };
