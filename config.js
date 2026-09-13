3e0e4b09cab5439da96ee10a727f9476b74124f3
Arefin leno <arefinanwar112@gmail.com>
2026-09-13 16:44:30 +0600

    Promote Ring500 to root, archive 70-pilot in pilot70/

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.HUMAN_EVAL = {
   // Ring500 Apps Script backend (bound to the ring500-human sheet).
-  scriptUrl: "https://script.google.com/macros/s/AKfycbwLOkyeIC3FSUnL2i6OYnCFyIaTKpi2nbX6VdR9VF8GfNrVEUfQM95WiKMx0od7icTktA/exec",
+  scriptUrl: "https://script.google.com/macros/s/AKfycbxHm7cdW0vdzOFsSZvmadC_qnWrS48Pz6P2H0WqAjdA3Xf9lPPmNlScYhjgw_uLOX2_xQ/exec",
 };
